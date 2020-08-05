e4af101e9869315fbd55d69843069fb00011da6a
kenneth260298 <k.lopez2698@gmail.com>
2020-08-05 07:31:58 -0600

    Terminado, ya no muestra el mensaje

--- DemoJacada/MainWindow.xaml.cs
+++ DemoJacada/MainWindow.xaml.cs
@@ -11,38 +11,45 @@ namespace DemoJacada
 
     public partial class MainWindow : Window
     {
-
+        private WebBrowser webBrowser;
 
         public MainWindow()
         {
             InitializeComponent();
-            JacadaBrowser.LoadCompleted += new LoadCompletedEventHandler(browser_LoadCompleted);
-            JacadaBrowser.Navigated += new NavigatedEventHandler(wbMain_Navigated);
+           
+           
         }
         private void abrir_Click(object sender, RoutedEventArgs e)
         {
-
-            JacadaBrowser.Navigate(new Uri("https://vivr.io/qz37X91"));
-            //JacadaBrowser.Source = new Uri("https://vivr.io/qz37X91");
-            JacadaBrowser.Visibility = Visibility.Visible;
+            WebBrowser wb = new WebBrowser();
+            wb.Margin = new Thickness(122, 38, 30, 10);
+            wb.Source = new Uri(txtUrl.Text);    //https://vivr.io/qz37X91
+            wb.Visibility = Visibility.Visible;
+         
+            this.webBrowser = wb;
+            this.webBrowser.LoadCompleted += new LoadCompletedEventHandler(browser_LoadCompleted);
+            
+            JacadaGrid.Children.Add(wb);
+         
         }
-
+        
         private void cerrar_Click(object sender, RoutedEventArgs e)
         {
-            JacadaBrowser.Visibility = Visibility.Hidden;
+            this.webBrowser.Dispose();
         }
 
+        
         public void browser_LoadCompleted(object sender, NavigationEventArgs args)
         {
             try
             {
-                FieldInfo webBrowserInfo = JacadaBrowser.GetType().GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
+                FieldInfo webBrowserInfo = this.webBrowser.GetType().GetField("_axIWebBrowser2", BindingFlags.Instance | BindingFlags.NonPublic);
 
                 object comWebBrowser = null;
                 object zoomPercent = 60;
                 if (webBrowserInfo != null)
                 {
-                    comWebBrowser = webBrowserInfo.GetValue(JacadaBrowser);
+                    comWebBrowser = webBrowserInfo.GetValue(this.webBrowser);
                 }
                 if (comWebBrowser != null)
                 {
@@ -56,43 +63,6 @@ namespace DemoJacada
             {
 
             }
-        }
-
-
-
-        void wbMain_Navigated(object sender, NavigationEventArgs e)
-        {
-            SetSilent(JacadaBrowser, false); // make it silent
-        }
-
-
-        public static void SetSilent(WebBrowser browser, Boolean silent)
-        {
-            if (browser == null)
-                throw new ArgumentNullException("browser");
-
-            // get an IWebBrowser2 from the document
-            IOleServiceProvider sp = browser.Document as IOleServiceProvider;
-            if (sp != null)
-            {
-                Guid IID_IWebBrowserApp = new Guid("0002DF05-0000-0000-C000-000000000046");
-                Guid IID_IWebBrowser2 = new Guid("D30C1661-CDAF-11d0-8A3E-00C04FC9E26E");
-
-                object webBrowser;
-                sp.QueryService(ref IID_IWebBrowserApp, ref IID_IWebBrowser2, out webBrowser);
-                if (webBrowser != null)
-                {
-                    webBrowser.GetType().InvokeMember("Silent", BindingFlags.Instance | BindingFlags.Public | BindingFlags.PutDispProperty, null, webBrowser, new object[] { silent });
-                }
-            }
-        }
-
-
-        [ComImport, Guid("6D5140C1-7436-11CE-8034-00AA006009FA"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        private interface IOleServiceProvider
-        {
-            [PreserveSig]
-            int QueryService([In] ref Guid guidService, [In] ref Guid riid, [MarshalAs(UnmanagedType.IDispatch)] out object ppvObject);
         }
     }
 }
